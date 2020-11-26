d24ceb27f3b7b5972fa6dfe0c7c689bddca41ada
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-11-26 20:21:04 +0800

    feature
    Dns解析工具

---
+++ Dns/Dns/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Net;
+
+//dotnet run 127.0.0.1 www.baidu.com sports.sina.com.cn localhost
+foreach (var ip in args)
+{
+    var hostEntry = Dns.GetHostEntry(ip);
+    System.Console.WriteLine($"主机名：{hostEntry.HostName}");
+    foreach (var address in hostEntry.AddressList)
+        System.Console.WriteLine($"  地址：{address}");
+    System.Console.WriteLine("-------------------------------------------");
+}
