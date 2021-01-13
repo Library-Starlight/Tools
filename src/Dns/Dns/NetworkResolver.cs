d07748b859b26f0dc8d197076b5a737d172c03e9
Ganwei_RD-09 <Ganwei_RD-09@LAPTOP-TFRH16I6>
2021-01-13 16:31:42 +0800

    feat：IPAddress

---
+++ src/Dns/Dns/NetworkResolver.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Net
+{
+    public class NetworkResolver
+    {
+        /// <summary>
+        /// 解析服务器网络接口
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<string> ResolveNetworkInterfaces()
+        {
+            foreach (NetworkInterface netInt in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                IPInterfaceProperties property = netInt.GetIPProperties();
+                foreach (UnicastIPAddressInformation ip in property.UnicastAddresses)
+                {
+                    if (ip.IsDnsEligible && ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                    {
+                        // Windows不包含
+                        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+                            && !ip.IsDnsEligible)
+                        {
+                            continue;
+                        }
+
+                        var ipaddr = ip.Address.ToString();
+                        yield return ipaddr;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 获取服务器网络接口
+        /// </summary>
+        /// <returns></returns>
+        public static List<IPAddress> GetHostNetworkInterface()
+        {
+            return System.Net.NetworkResolver
+                .ResolveNetworkInterfaces()
+                .Select(s => IPAddress.Parse(s))
+                .ToList();
+        }
+    }
+}
--- src/Dns/Dns/Program.cs
+++ src/Dns/Dns/Program.cs
@@ -4,11 +4,18 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Runtime.InteropServices;
+using static System.Net.NetworkResolver;
 
 //dotnet run 127.0.0.1 www.baidu.com sports.sina.com.cn localhost
 
 var ips = ResolveNetworkInterfaces();
 foreach (var item in ips)
+{
+    Console.WriteLine(item);
+}
+
+var ipaddresses = GetHostNetworkInterface();
+foreach (var item in ipaddresses)
 {
     Console.WriteLine(item);
 }
@@ -22,28 +29,5 @@ void ResoveByHostName()
         foreach (var address in hostEntry.AddressList)
             System.Console.WriteLine($"  地址：{address}");
         System.Console.WriteLine("-------------------------------------------");
-    }
-}
-
-IEnumerable<string> ResolveNetworkInterfaces()
-{
-    foreach (NetworkInterface netInt in NetworkInterface.GetAllNetworkInterfaces())
-    {
-        IPInterfaceProperties property = netInt.GetIPProperties();
-        foreach (UnicastIPAddressInformation ip in property.UnicastAddresses)
-        {
-            if (ip.IsDnsEligible && ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
-            {
-                // Windows不包含
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
-                    && !ip.IsDnsEligible)
-                {
-                    continue;
-                }
-
-                var ipaddr = ip.Address.ToString();
-                yield return ipaddr;
-            }
-        }
     }
 }
